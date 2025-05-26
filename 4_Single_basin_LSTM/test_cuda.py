828048ef413d6a009b571dbad06ff2d1bde40a37
David De Knop <ddknop@GrNete.UGent.be>
2025-05-26 19:55:18 +0200

    Remove 4_Single_basin_LSTM folder

--- 4_Single_basin_LSTM/test_cuda.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    print("CUDA is available. You have a CUDA-capable GPU.")
-else:
-    print("CUDA is not available. You do not have a CUDA-capable GPU.")
